2469c25e8329c84b79c0e401d4619500d448c6a0
naman0058 <42539761+naman0058@users.noreply.github.com>
2024-06-14 15:09:20 +0530

    Update pool.js

--- routes/pool.js
+++ routes/pool.js
@@ -3,9 +3,9 @@ var mysql = require('mysql2')
 
 const pool = mysql.createPool({
 
-  host : 'db-mysql-blr1-78922-do-user-4199968-0.c.db.ondigitalocean.com',
+  host : 'db-mysql-blr1-30615-do-user-16192783-0.c.db.ondigitalocean.com',
    user: 'doadmin',
-  password:'AVNS_GQrps4okJNd-Q3VSn68',
+  password:'AVNS_kkPKnkohsUpl8Sa1iL2',
     database: 'trading_app_management',
     port:'25060' ,
     multipleStatements: true,
